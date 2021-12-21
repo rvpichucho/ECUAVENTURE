13702f6577fb5819fc2af4ee59f2be8c49c92529
rvpichucho <63176305+rvpichucho@users.noreply.github.com>
2021-12-21 12:11:08 -0500

    Cambios

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 
 //import 'package:ecuaventure/src/pages/home_page.dart';
+//import 'package:ecuaventure/src/pages/inicio_page.dart';
 import 'package:ecuaventure/src/pages/menu_page.dart';
+import 'package:ecuaventure/src/providers/provider_moto.dart';
 
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -14,18 +16,28 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false, //PARA QUITAR EL DEBUG DE LA APP
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create:(_)=>MotoProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false, //PARA QUITAR EL DEBUG DE LA APP
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: 'home',
+        routes: {
+          'home':(context)=> const Menu()
+        },
+       //home:Scaffold(
+          //appBar:AppBar(title: const Text("Motos")) ,body: const Menu()),
+      
       ),
-     home:Scaffold(
-        appBar:AppBar(title: const Text("Motos")) ,body: const Menu()),
-    
     );
   }
 }
+
 
 
   
